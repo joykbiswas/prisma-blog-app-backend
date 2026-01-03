5babc287a4e2bc9991fdc44f2b49e879aa2837fe
joykbiswas <joykumarbiswas100@gmail.com>
2026-01-03 22:02:52 +0600

    Searching and filter funclity add

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,25 +1,63 @@
 import { Request, Response } from "express";
 import { postService } from "./post.service";
+import { PostStatus } from "../../../generated/prisma/enums";
 
-const createPost = async (req:Request, res:Response ) =>{
-   try {
+const createPost = async (req: Request, res: Response) => {
+  try {
     const user = req.user;
-    if(!user){
-       return res.status(400).json({
-            error: "Unauthorized"
-        })
+    if (!user) {
+      return res.status(400).json({
+        error: "Unauthorized",
+      });
     }
-    
-    const result = await postService.createPost(req.body, user.id as string )
-    res.status(201).json(result)
-   } catch (err) {
+
+    const result = await postService.createPost(req.body, user.id as string);
+    res.status(201).json(result);
+  } catch (err) {
     res.status(400).json({
-        error: "Post create failed",
-        details: err
-    })
-   }
-}
+      error: "Post create failed",
+      details: err,
+    });
+  }
+};
+
+const getAllPost = async (req: Request, res: Response) => {
+  try {
+    const { search } = req.query;
+    const searchString = typeof search === "string" ? search : undefined;
+    const tags = req.query.tags ? (req.query.tags as string).split(",") : [];
+
+    const isFeatured = req.query.isFeatured
+      ? req.query.isFeatured === "true"
+        ? true
+        : req.query.isFeatured === "false"
+        ? false
+        : undefined
+      : undefined;
+
+      const status = req.query.status as PostStatus | undefined;
+
+      const authorId = req.query.authorId as string | undefined;
+
+    console.log({ isFeatured });
+
+    const result = await postService.getAllPost({
+      search: searchString,
+      tags,
+      isFeatured,
+      status,
+      authorId
+    });
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(400).json({
+      error: "Post create failed",
+      details: err,
+    });
+  }
+};
 
 export const PostController = {
-    createPost
-}
+  createPost,
+  getAllPost,
+};
--- src/modules/post/post.router.ts
+++ src/modules/post/post.router.ts
@@ -5,6 +5,8 @@ import auth, { UserRole } from '../../middlewares/auth';
 
 const router = express.Router();
 
+router.get("/", PostController.getAllPost)
+
 
 router.post("/",
     auth(UserRole.USER),
